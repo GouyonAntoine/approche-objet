76cbe52fc6128c0691d890770c71e8b9572eb566
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-10-08 13:57:10 +0200

    TP8
    TP8 du jour 1 terminé

--- approche-objet/src/fr/diginamic/banque/TestOperation.java
+++ approche-objet/src/fr/diginamic/banque/TestOperation.java
@@ -16,7 +16,7 @@ public class TestOperation {
         Operation credit1 = new Credit("11/12/2021", 100);
         Operation credit2 = new Credit("12/12/2021", 200);
         Operation debit1 = new Debit("13/12/2021", 50);
-        Operation debit2 = new Debit("14/12/2021", 100);
+        Operation debit2 = new Debit("14/12/2021", 600);
 
         listOp.add(credit1);
         listOp.add(credit2);
@@ -27,5 +27,22 @@ public class TestOperation {
             System.out.println("Information sur l'Opération n°" + i + " qui est un " + listOp.get(i).getType() + " => " + listOp.get(i).toString());
         }
         
+        double montantOperation = 0;
+        
+        for (int i=0; i<listOp.size(); i++) {
+            System.out.println(listOp.get(i).getType() + "sur le compte de : " + listOp.get(i).getMontantOperation());
+            if(listOp.get(i).getType() == "Crédit"){
+                montantOperation += listOp.get(i).getMontantOperation();
+            }
+            else {
+                montantOperation -= listOp.get(i).getMontantOperation();
+            }
+        }
+        if(montantOperation >= 0){
+            System.out.println("Votre solde est positif de : " + montantOperation);
+        }
+        else {
+            System.out.println("Votre solde est à découvert de : " + montantOperation);
+        }
     }
 }
